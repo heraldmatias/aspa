edcdd51aee0dcf66015e5f83a9f07c25dcbd1e3b
Herald Olivares <heraldo@jorge-HP-Compaq-8200-Elite-SFF-PC.(none)>
2012-09-30 18:28:14 -0500

    fixes

--- static/js/prueba.js
+++ static/js/prueba.js
@@ -1,6 +1,6 @@
 function andina_news(){        
         var noticias = $("#noticias");
-	$.getJSON('http://50.116.43.119:8070/andina/noticias/json/', function(data){
+	$.getJSON('/andina/noticias/json/', function(data){
                 $.each(data, function(key,value){                
 			noticias.append(key +" = " + value["titular"]);
 		});
