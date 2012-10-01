6c8dc6fbc48cb30b63524a6bc5051fc853186aef
Herald Olivares <heraldo@jorge-HP-Compaq-8200-Elite-SFF-PC.(none)>
2012-10-01 14:16:19 -0500

    andina

--- static/js/prueba.js
+++ static/js/prueba.js
@@ -1,16 +1,29 @@
-function andina_news(){        
-        var noticias = $("#noticias");
-	$.getJSON('/andina/noticias/json/', function(data){
-                $.each(data, function(key,value){                
-			noticias.append(key +" = " + value["titular"]);
-		});
-		
+
+function callback(data){        
+        var noticias = $('#andina');
+        var contenido = '';
+//	noticias.css('background-color','green');
+        $.each(data, function(key,value){
+		contenido = contenido + noticia(key,value['titular'],value['texto']);
 	});
+        noticias.html(contenido);
 }
+
+function noticia(num,titulo, texto){
+var nota= "<div class='accordion-group'> <div class='accordion-heading'> <a class='accordion-toggle'  data-toggle='collapse' data-parent='#noticias-aspa' href='#noticia"+num+"'>"+ titulo +" </a> </div>  <div style='height: 0px;' id='noticia"+num+"' class='accordion-body collapse'>  <div class='accordion-inner'>"+texto+"</div></div></div>";
+return nota;
+}
+
+function jsonp(url){  
+    var head = document.getElementsByTagName('head')[0];  
+    var script = document.createElement('script');  
+    script.type = 'text/javascript';  
+    script.src = url;  
+    head.appendChild(script);  
+//$('#noticias').css('background-color','red');
+} 
 /*
 $.each(data, function(key,value){                
 			noticias.append(key);
 		});*/
-$(document).ready(function() {
-andina_news();
-});
+
